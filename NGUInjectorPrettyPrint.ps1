1b97e26d972265360c701de94bec7c39f2735307
WookieeNo1 <wookieeno1-GitHub@yahoo.com>
2021-05-11 18:28:57 +0100

    No Overall Code Change - Just Tidied

--- NGUInjectorPrettyPrint.ps1
+++ NGUInjectorPrettyPrint.ps1
@@ -1,8 +1,7 @@
-param ($LogFile=$args[0], $DisplayMode=$args[1])
+param ($LogFile = $args[0], $DisplayMode = $args[1])
 
 #Fix for $host.ui.RawUI.WindowTitle = $BaseFile + ' Parser' Exception
-if ($PSVersionTable.PSversion.major -lt 7) 
-{
+if ($PSVersionTable.PSversion.major -lt 7) {
     Write-Host "Requires PowerShell v7"
     exit
 }
@@ -12,13 +11,11 @@ Import-Module -Force $configFullPath
 
 #Import User-Defined Variables from validated list
 # Added clrHyperBole - set to 0 for make it disappear
-$ValidVariables = @("clrMoneyPitReward","clrHyperbole")
-if (Test-Path -Path $PSScriptRoot\Colours.csv )
-{
-    Import-CSV -Path $PSScriptRoot\Colours.csv|Foreach-Object -Process {
+$ValidVariables = @("clrMoneyPitReward", "clrHyperbole")
+if (Test-Path -Path $PSScriptRoot\Colours.csv ) {
+    Import-CSV -Path $PSScriptRoot\Colours.csv | Foreach-Object -Process {
 
-        if ($PSItem.Variable -in $ValidVariables)
-        {
+        if ($PSItem.Variable -in $ValidVariables) {
             Remove-Variable -name $PSItem.Variable
             New-Variable -Name $PSItem.Variable -Value $PSItem.Value 
         }
@@ -32,30 +29,28 @@ try {
     $ValidFiles = @("inject.log", "pitspin.log", "loot.log")
     $ValidModes = @("full", "tail")
 
-    $RanFromParam=$false
+    $RanFromParam = $false
 
     $Old_Title = $host.ui.RawUI.WindowTitle
 
-    if ($LogFile -and $LogFile -in $ValidFiles)
-    {
-        $BaseFile=$LogFile
-        if ($DisplayMode -and $DisplayMode -in $ValidModes){
-            $FileName = $Env:Userprofile+"\Desktop\NGUInjector\logs\"+$BaseFile
+    if ($LogFile -and $LogFile -in $ValidFiles) {
+        $BaseFile = $LogFile
+        if ($DisplayMode -and $DisplayMode -in $ValidModes) {
+            $FileName = $Env:Userprofile + "\Desktop\NGUInjector\logs\" + $BaseFile
 
-            $RanFromParam=$true
+            $RanFromParam = $true
 
             $host.ui.RawUI.WindowTitle = $BaseFile + ' Parser'
 
-            if ($DisplayMode -eq $ValidModes[0]){
-                Get-Content $FileName | ForEach-Object{ ProcessLines }
+            if ($DisplayMode -eq $ValidModes[0]) {
+                Get-Content $FileName | ForEach-Object { ProcessLines }
             }
             else {
-                Get-Content $FileName -Tail 2 -Wait| ForEach-Object{ ProcessLines } 
+                Get-Content $FileName -Tail 2 -Wait | ForEach-Object { ProcessLines } 
             }
         }
     }
-    if ($RanFromParam)
-    {
+    if ($RanFromParam) {
         Read-Host -Prompt "Press Enter to Exit" -MaskInput
     }
     else {
@@ -64,6 +59,6 @@ try {
 }
 finally {
     Write-host
-    Write-host "Resetting title to",$Old_Title
-    $host.ui.RawUI.WindowTitle=$Old_Title
+    Write-host "Resetting title to", $Old_Title
+    $host.ui.RawUI.WindowTitle = $Old_Title
 }
